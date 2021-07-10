2f1752ddbb4a285b60292b05e88ee5ab2198616d
Jordan Onwodi <81950260+Jonwodi@users.noreply.github.com>
2021-07-10 19:11:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+The Richest Man in Babylon by George S. Clason
+"""
+# ====================================================
+success = 'Success means accomplishments as the result of our own efforts and abilities.'
+
+print(success)
+# ====================================================
+"""
+Seven cures for a lean purse
+"""
+# ====================================================
+"""
+The first cure: A part of all you earn is yours to keep & start thy purse to fattening.
+"""
+
+the_first_cure = "\nGet a job, save a minimum of 10% of your earnings, no matter how little you earn. You can save more than 10% of your earnings if you can afford to. It will be used for income-bringing investments."
+
+print(the_first_cure)
+# ====================================================
+"""
+The second cure: control thy expenditures
+"""
+
+the_second_cure = "\nBudget the remaining 90% or less of earnings and spend it on neccessary expenses and other things that are possible, so as long as you don't spend over this remaining amoumt of earnings. Don't ever use or spend the first 10% of earnings you're saving for income-bringing investments. let your motto be 100% of appreciated value for each coin spent. Continue to make suitable adjustments to allow have enough to spend on neccessary expenses and other desires using only the remaining 90% or less of earnings."
+
+print(the_second_cure)
+# ====================================================
+"""
+The third cure: make thy gold multiply
+"""
+
+the_third_cure = "\nInvest the first part of your earnings (10% or more of earnings) that you have been saving into income-bringing invetsments."
+
+print(the_third_cure)
+# ====================================================
